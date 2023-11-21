833f3b34b346d4241cacaf58a897986a2c72e447
NickyMatthijssen <36468434+NickyMatthijssen@users.noreply.github.com>
2023-11-21 12:23:46 +0100

    Add correct env

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,9 +1,8 @@
+import { env } from '$env/dynamic/public';
 import { Client, Account, Databases, ID } from 'appwrite';
 
 export const client = new Client();
-client
-	.setEndpoint('https://cloud.appwrite.io/v1')
-	.setProject(import.meta.env.PUBLIC_APPWRITE_PROJECT_ID);
+client.setEndpoint('https://cloud.appwrite.io/v1').setProject(env.PUBLIC_APPWRITE_PROJECT_ID);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
